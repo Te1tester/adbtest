32fa402814170dfef4c0d6a7445d973ed112cb74
Te1tester <107201091+Te1tester@users.noreply.github.com>
2025-05-18 03:03:21 +0700

    Create AdbWrapper.cs

---
+++ AdbWrapper.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+
+class AdbWrapper {
+    static void Main(string[] args) {
+        string logLine = $"{DateTime.Now}: adb {string.Join(" ", args)}";
+        File.AppendAllText("C:\\jeb_adb_log.txt", logLine + Environment.NewLine);
+
+        Process adb = new Process();
+        adb.StartInfo.FileName = "adb_real.exe";
+        adb.StartInfo.Arguments = string.Join(" ", args);
+        adb.StartInfo.UseShellExecute = false;
+        adb.StartInfo.RedirectStandardOutput = true;
+        adb.StartInfo.RedirectStandardError = true;
+
+        adb.Start();
+        Console.Write(adb.StandardOutput.ReadToEnd());
+        Console.Error.Write(adb.StandardError.ReadToEnd());
+        adb.WaitForExit();
+        Environment.Exit(adb.ExitCode);
+    }
+}
